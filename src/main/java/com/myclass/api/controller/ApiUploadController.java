79e358732eacf3bbbdd2ed7a44015cfab8935779
nguyenhuynhminhthuan265 <mty2605@gmail.com>
2019-12-19 13:16:27 +0700

    cloudinary UPLOAD

--- src/main/java/com/myclass/api/controller/ApiUploadController.java
+++ src/main/java/com/myclass/api/controller/ApiUploadController.java
@@ -1,8 +1,7 @@
 package com.myclass.api.controller;
 import java.io.File;
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
+import java.io.FileOutputStream;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.myclass.entity.Photo;
-import com.myclass.entity.User;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.myclass.repository.PhotoRepository;
 import com.myclass.repository.UserRepository;
 
@@ -31,52 +30,76 @@ public class ApiUploadController {
 	@PostMapping("/upload")
 	@CrossOrigin(origins = "*")
 	public Object post(@RequestParam(name = "file") MultipartFile file, String email, String password) {
-		try {
-			System.out.println("Email: " + email);
-			System.out.println("Password: " + password);
-			// Lấy đường dẫn tuyệt đối của thư mục chứa file upload
-			// String folderPath = request.getServletContext().getRealPath("/upload/");
-			//String folderPath = System.getProperty("user.dir");
-			// String folderPath = "";
-			String folderPath = "/src/main/resources/static/assets" + "/upload/";
-			// Sử dụng đối tượng File của java.io để kiểm tra thư mục
-			System.out.println("Folder save image: " + folderPath);
-			File dir = new File(folderPath);
-			// Nếu chưa tồn tại thư mục upload thì tạo mới
-			if (!dir.exists()) {
-				dir.mkdir();
-			}
-			// Lấy ra tên file vừa gửi từ client lên
-			String fileName = file.getOriginalFilename();
-			// Sử dụng đối tượng File của java.io để lưu file
-			File filePath = new File(folderPath + fileName);
-			System.out.println("filepath: " + filePath.getAbsolutePath());
-			User user = null;
-			user = userRepository.findByEmail(email);
-			if (user == null) {
-				System.out.println("Lỗi null");
-				return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
-			}
-			if (user != null) {
-				System.out.println("User in database: " + user.toString());
-				if (!user.getPassword().equals(password)) {
-					return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
-				}
-			}
-			// Sử dụng phương thức transferTo của MultipartFile để lưu file xuống thư mục
-			file.transferTo(filePath);
-			// int id=photoRepository.findAll().size();
-			// System.out.println("Length photo: " + id);
-			int userIdUpload = user.getId();
-			Photo photo = new Photo(fileName, filePath.toString(), userIdUpload);
-			photoRepository.save(photo);
-			System.out.println("Đã save file");
-			return new ResponseEntity<String>("/upload/" + fileName, HttpStatus.OK);
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+//		try {
+//			System.out.println("Email: " + email);
+//			System.out.println("Password: " + password);
+//			// Lấy đường dẫn tuyệt đối của thư mục chứa file upload
+//			// String folderPath = request.getServletContext().getRealPath("/upload/");
+//			//String folderPath = System.getProperty("user.dir");
+//			// String folderPath = "";
+//			String folderPath = "/src/main/resources/static/assets" + "/upload/";
+//			// Sử dụng đối tượng File của java.io để kiểm tra thư mục
+//			System.out.println("Folder save image: " + folderPath);
+//			File dir = new File(folderPath);
+//			// Nếu chưa tồn tại thư mục upload thì tạo mới
+//			if (!dir.exists()) {
+//				dir.mkdir();
+//			}
+//			// Lấy ra tên file vừa gửi từ client lên
+//			String fileName = file.getOriginalFilename();
+//			// Sử dụng đối tượng File của java.io để lưu file
+//			File filePath = new File(folderPath + fileName);
+//			System.out.println("filepath: " + filePath.getAbsolutePath());
+//			User user = null;
+//			user = userRepository.findByEmail(email);
+//			if (user == null) {
+//				System.out.println("Lỗi null");
+//				return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+//			}
+//			if (user != null) {
+//				System.out.println("User in database: " + user.toString());
+//				if (!user.getPassword().equals(password)) {
+//					return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+//				}
+//			}
+//			// Sử dụng phương thức transferTo của MultipartFile để lưu file xuống thư mục
+//			file.transferTo(filePath);
+//			// int id=photoRepository.findAll().size();
+//			// System.out.println("Length photo: " + id);
+//			int userIdUpload = user.getId();
+//			Photo photo = new Photo(fileName, filePath.toString(), userIdUpload);
+//			photoRepository.save(photo);
+//			System.out.println("Đã save file");
+//			return new ResponseEntity<String>("/upload/" + fileName, HttpStatus.OK);
+//		} catch (IllegalStateException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		
+		String cloudinaryImgURL=null;
+	    try {
+	        File fileDir = new File("rowFiles");
+	        if (! fileDir.exists()){
+	            fileDir.mkdir();
+	        }
+	        String fileName=file.getOriginalFilename();
+	        File physicalFile=new File(file.getOriginalFilename());
+	        FileOutputStream fout=new FileOutputStream(fileDir.getName()+"/"+physicalFile);
+	        fout.write(file.getBytes());
+	        fout.close();
+	        File toUpload = new File("rowFiles/"+fileName);
+	        Cloudinary cloudinary = new Cloudinary();
+	        System.out.println("API Key:"+cloudinary.config.apiKey);
+	        Map params = ObjectUtils.asMap("public_id", "SRWRestImageBase/"+fileName);
+	        Map uploadResult = cloudinary.uploader().upload(toUpload, params);
+	        toUpload.delete();
+	        System.out.println("==============>>"+uploadResult.get("url"));
+	        cloudinaryImgURL=uploadResult.get("url").toString();
+	    } catch (Exception e) {
+	        System.out.println("upload:"+e.getMessage());
+	    }
+	    return new ResponseEntity<Object>("File uploaded successfully:"+cloudinaryImgURL,HttpStatus.OK);
 	}
 }
